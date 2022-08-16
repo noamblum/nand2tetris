5cc624c8d55d10af36869200db9a1e59234fcdc4
Ofek Shabo <ofek_shabo@cs.huji.ac.il>
2022-08-16 22:43:19 +0300

    Finished fill.asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,67 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// Pseudo code:
+// key = getInput()
+// if key == 0:
+//      fillValue = 0
+// else:
+//      fillValue = -1
+// for all screen:
+//   set to fillValue
+// goto start
+
+(MAIN_LOOP)
+    @KBD
+    D=M;
+
+    @BLACK
+    D;JNE
+(WHITE)
+    @fillValue
+    M=0;
+    @CHECK_MATCH
+    0;JMP
+
+(BLACK)
+    @fillValue
+    M=-1;
+
+(CHECK_MATCH) // Checks if the top-right pixel is filled with the correct value
+    @fillValue
+    D=M;
+    @SCREEN
+    D = M-D;
+    @DRAW_LOOP_START
+    D;JNE
+    @MAIN_LOOP
+    0;JMP
+
+
+(DRAW_LOOP_START)
+    @SCREEN
+    D=A;
+    @curInd
+    M=D;
+(DRAW_LOOP)
+    @fillValue
+    D=M;
+    @curInd
+    A=M;
+    M=D;
+
+    // Go to next pixel
+    D=A + 1;
+    @curInd
+    M=D;
+
+    // Check stop iteration
+    @KBD
+    D = D - A;
+    @DRAW_LOOP
+    D;JNE
+
+@MAIN_LOOP
+0;JMP
+    
