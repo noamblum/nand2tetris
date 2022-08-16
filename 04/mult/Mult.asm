fca1159fefda0f9e976624dc81dcd96b6b21b31a
Ofek Shabo <ofek_shabo@cs.huji.ac.il>
2022-08-16 21:48:13 +0300

    Finished mult.asm

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -10,3 +10,75 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// Psuedocode for mult x * y:
+// mask = 1
+// for i = 0 to 15:
+//    tmp = mask & y (checking bit is active)
+//    if tmp != 0:
+//       sum += x
+//    x = x + x
+//    mask = mask + mask
+// return sum
+
+@i
+M=0;
+
+@mask
+M=1;
+
+// Copy first variable
+@R0
+D=M;
+@x
+M=D;
+
+// Make sure R2 starts as 0
+@R2
+M=0;
+
+(LOOP)
+    @mask
+    D = M;
+    @R1
+    D = M & D;
+
+    // Check mask != 0
+    @LOOP_UPDATE
+    D;JEQ
+
+    @x
+    D = M;
+
+    @R2
+    M = M + D;
+
+(LOOP_UPDATE)
+    @x
+    D = M;
+    @tmp
+    M = D;
+    @x
+    M = M + D;
+
+    @mask
+    D = M;
+    @tmp
+    M = D;
+    @mask
+    M = M + D;
+
+@15 // Max iteration
+D=A;
+@i
+D = M - D;
+@END
+D;JEQ
+@i
+M = M + 1;
+@LOOP
+0;JMP
+
+(END)
+    @END
+    0;JMP
