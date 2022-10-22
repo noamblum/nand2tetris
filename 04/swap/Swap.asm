6cf6889da105b4241f4e21a46042237a1a398e85
Noam Blum <noamblum756@gmail.com>
2022-10-22 12:12:17 +0300

    Finished Ex4

--- 04/swap/Swap.asm
+++ 04/swap/Swap.asm
@@ -14,4 +14,111 @@
 // Requirements:
 // - Changing R14, R15 is not allowed.
 
-// Put your code here.
+// Initialize variables
+// i = 0
+@0
+D=A
+@i
+M=D
+
+// maxAddr = R14 & minAddr = R14
+@R14
+D=M
+@maxAddr
+M=D
+@minAddr
+M=D
+
+
+(LOOP)
+    @i
+    D=M
+    @R15
+    D = D-M
+    @LOOP_END
+    D;JEQ
+        // Access the i-th element in the array
+        @i
+        D=M
+        @R14
+        A=M+D
+        D=M
+        // Check Array[i] >= max
+        @maxAddr
+        A=M
+        D=D-M
+        
+        // If Array[i] < max, skip updating it
+        @UPDATE_MIN
+        D;JLT
+        // D = *R14 + i
+        @R14
+        D=M
+        @i
+        D=D+M
+        @maxAddr
+        M=D
+
+        (UPDATE_MIN)
+        // Access the i-th element in the array
+        @i
+        D=M
+        @R14
+        A=M+D
+        D=M
+        // Check Array[i] <= min
+        @minAddr
+        A=M
+        D=M-D
+
+        // If Array[i] > min, skip updating it
+        @LOOP_UPDATE
+        D;JLT
+        // D = *R14 + i
+        @R14
+        D=M
+        @i
+        D=D+M
+        @minAddr
+        M=D
+
+
+
+    (LOOP_UPDATE)
+    // i++
+    @i
+    M=M+1
+
+    @LOOP
+    0;JMP
+(LOOP_END)
+
+
+// Swap
+// tmp = *maxAddr
+@maxAddr
+A=M
+D=M
+@tmp
+M=D
+
+// *maxAddr = *minAddr
+@minAddr
+A=M
+D=M
+@maxAddr
+A=M
+M=D
+
+// *minAddr = tmp
+@tmp
+D=M
+@minAddr
+A=M
+M=D
+
+
+(END_PROGRAM)
+@END_PROGRAM
+0;JMP
+
